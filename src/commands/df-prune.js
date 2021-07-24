bbb9ed0e4b20e5a5f316bfd07489ad367cdabde5
Antonio Jiménez Pastor <antonio.jimenez@goldenrace.local>
2021-07-25 00:27:22 +0200

    refactor clean -> prune

--- src/commands/df-prune.js
+++ src/commands/df-prune.js
@@ -6,15 +6,15 @@ const command = Object.keys(package.bin)[1];
 const app = program
   .name(command)
   .version(package.version)
-  .description("Docker function clean")
+  .description("Docker function prune")
   .action((package, options) => {
-    require("./clean")(package, program.opts());
+    require("./prune")(package, program.opts());
   })
   .on("--help", () => {
     console.log(`
 -----------------------------------------------
 Help:
-  Clean containers, images and volumes not used.
+  Prune containers, images and volumes not used.
     `);
   });
 
